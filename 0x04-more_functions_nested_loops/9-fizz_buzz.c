47c389104649181e8d0b11be32adbc6f040b65d1
ahamza2 <hamzaaarab78@gmail.com>
2023-06-24 08:05:57 -0700

    fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * main - prints the numbers from 1 to 100, followed by a new line.
- *
+ * main - prints the numbers from 1 to 100, followed by a new line
  * Return: return 0
  */
 
 int main(void)
 {
-	int i;
 
-	i =1;
+	int i = 1;
+
 	while (i <= 100)
 	{
 		if (i % 5 == 0 && i % 3 == 0)
